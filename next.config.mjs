0bb027ed3deb4c0162cf11bc980674afbfdfaae8
sky <akash.ap111@outlook.com>
2025-01-22 11:43:17 +0530

    npm

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "images.unsplash.com", // already used for other images
+      "assets.aceternity.com", // add this line to allow the new hostname
+    ],
+  },
+};
 
 export default nextConfig;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,53 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const { nextui } = require("@nextui-org/react");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+function addVariablesForColors({ addBase, theme }) {
+  const colors = theme("colors");
+  if (!colors) {
+    throw new Error("No colors found in the theme.");
+  }
+
+  const allColors = flattenColorPalette(colors);
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
+    "./app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui(), addVariablesForColors],
 };
